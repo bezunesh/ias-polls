fcf009cba8875bd56e9aec747e74364485413b7a
Bezunesh <zueb21@gmail.com>
2021-04-09 17:19:59 -0400

    added oauth scopes2

--- main.tf
+++ main.tf
@@ -93,5 +93,8 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
